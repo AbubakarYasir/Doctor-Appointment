efd7f47eccd3e8c7991644557e69a2b5fc5ef3be
Abubakar Yasir <theabubakaryasir@gmail.com>
2023-04-09 06:48:39 +0500

    setting up Database - MongoDB

---
+++ config/dbConfig.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/doctorsApp", { useNewUrlParser: true, useUnifiedTopology: true });
+
+module.exports = mongoose;
